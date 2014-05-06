bce99d9c046c9ef18f4f5a1816a54c78131522f1
Miško Hevery <misko@hevery.com>
2014-05-06 15:38:57 -0700

    Update start-hook.sh

--- start-hook.sh
+++ start-hook.sh
@@ -8,4 +8,4 @@ cd "$(dirname "$0")"
 #
 # See: https://github.com/Coreh/hookshot
 #
-hookshot -p 9000 "git fetch && git reset --hard origin/generated"
+hookshot -p 9000 "git fetch && git checkout -f origin/generated"
